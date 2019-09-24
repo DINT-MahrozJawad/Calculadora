dbfb55940a49a287569cbc094f5e5c3f363a7f34
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-09-24 12:55:08 +0200

    Agregar archivos de proyecto.

---
+++ Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked_Suma(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_Resta(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_Multiplicacion(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_Division(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
